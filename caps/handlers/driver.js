f90745fd3cc02261c2a3402bfdb05e0e619207a4
Keelen Fisher <keelencarrera@gmail.com>
2022-10-03 23:49:39 -0500

    one duplication error

--- caps/handlers/driver.js
+++ caps/handlers/driver.js
@@ -3,7 +3,7 @@
 let eventDrive = require('../eventDrive');
 
 module.exports = (payload) => {
-  console.log(`Order is being picked up: ${payload.order.orderId}`);
+  console.log(`Order is being picked up: ${payload.orderId}`);
   console.log(`Order is now in transit`, payload);
   console.log(`Order is officially delivered, ${payload.order.orderId}`);
 };
